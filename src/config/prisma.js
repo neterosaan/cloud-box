f446e1587e825c9b1f42f62d5567d8d94f28de0e
neterosaan <lordnetero211@hotmail.com>
2026-06-04 21:32:21 +0300

    clean start

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,21 +1,9 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-// 1. Create a function to generate the client
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        // Explicitly use the Pooled Port (6543) for the application
-        url: process.env.DATABASE_URL, 
-      },
-    },
-  });
-};
-
-// 2. Ensure we only have ONE instance globally during development
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 export default prisma;
 
